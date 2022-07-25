ba8520c41fc90c61bc87d6bbd69f10227dd2e84c
alimulas20 <98461025+alimulas20@users.noreply.github.com>
2022-07-25 14:21:32 +0300

    implementation of ScoreBoard Scene

---
+++ Assets/scripts/ScoreBoard.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    public Text Score;
+    public Text Level;
+    public Text Correct;
+    public Text Accuracy;
+    void Start()
+    {
+        Score.text = timerCont.Score.ToString();
+        Level.text = timerCont.mult.ToString();
+        Correct.text = timerCont.QuesCount - timerCont.wrongCount + " of " + timerCont.QuesCount;
+        Accuracy.text = (100*(timerCont.QuesCount - timerCont.wrongCount) / timerCont.QuesCount).ToString();
+
+    }
+    public void next()
+    {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+   
+}
